371d60a9fde793b3ec400c297c5687ee9e71d49e
felicio.developer@gmail.com <logica@20>
2021-02-05 09:42:30 -0300

    Alteração

--- src/repositories/posts.go
+++ src/repositories/posts.go
@@ -9,49 +9,50 @@ import (
 type Posts struct {
 	db *gorm.DB
 }
-func NewRepositoryPosts(db *gorm.DB)*Posts{
+
+func NewRepositoryPosts(db *gorm.DB) *Posts {
 	return &Posts{db}
 }
 
-func(p *Posts)Create(post models.Posts)(models.Posts,error){
-	if err := p.db.Create(&post).Error;err != nil {
-		return models.Posts{},err
+func (p *Posts) Create(post models.Posts) (models.Posts, error) {
+	if err := p.db.Create(&post).Error; err != nil {
+		return models.Posts{}, err
 	}
-	return post,nil
+	return post, nil
 }
-func(p *Posts)Find(ID uint)(models.Posts,error){
+func (p *Posts) Find(ID uint) (models.Posts, error) {
 	var posts models.Posts
-	if err := p.db.Preload(clause.Associations).Find(&posts,ID).Error; err != nil{
-		return models.Posts{},err
+	if err := p.db.Preload(clause.Associations).Find(&posts, ID).Error; err != nil {
+		return models.Posts{}, err
 	}
-	return posts,nil
+	return posts, nil
 }
-func(p *Posts)Update(posts models.Posts, IDPost uint,ID uint)error{
-	if err := p.db.Model(posts).Where("id = ? and author_id = ?",IDPost,ID).
-		Updates(posts).Error; err != nil{
-		return err
-	}
-	return nil
-}
-func(p *Posts)List(param string)[]models.Posts{
+func (p *Posts) List(param string) []models.Posts {
 	var posts []models.Posts
-	p.db.Where("title like ?","%"+param+"%").Preload(clause.Associations).Find(&posts)
+	p.db.Where("title like ?", "%"+param+"%").Preload(clause.Associations).Find(&posts)
 	return posts
 }
-func(p *Posts)Delete(ID uint)error{
-	if err := p.db.Delete(&models.Posts{},"id = ?",ID).Error; err != nil{
+func (p *Posts) Update(posts models.Posts, IDPost uint, ID uint) error {
+	if err := p.db.Model(posts).Where("id = ? and author_id = ?", IDPost, ID).
+		Updates(posts).Error; err != nil {
 		return err
 	}
 	return nil
 }
-func(p *Posts)Like(IDPost,ID uint)error{
-	if err := p.db.Create(models.Likes{UsersID: ID, PostsID: IDPost}).Error; err != nil{
+func (p *Posts) Delete(ID uint) error {
+	if err := p.db.Delete(&models.Posts{}, "id = ?", ID).Error; err != nil {
 		return err
 	}
 	return nil
 }
-func(p *Posts)UnLike(IDPost, ID uint)error{
-	if err := p.db.Unscoped().Delete(models.Likes{},"posts_id = ? and users_id = ?",IDPost,ID).Error;err != nil{
+func (p *Posts) Like(IDPost, ID uint) error {
+	if err := p.db.Create(models.Likes{UsersID: ID, PostsID: IDPost}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+func (p *Posts) UnLike(IDPost, ID uint) error {
+	if err := p.db.Unscoped().Delete(models.Likes{}, "posts_id = ? and users_id = ?", IDPost, ID).Error; err != nil {
 		return err
 	}
 	return nil
